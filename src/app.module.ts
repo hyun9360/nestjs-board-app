50a5237946caf7100f86bce8a57369517ef84249
hyun <zvg7777@gmail.com>
2023-12-25 20:47:20 +0900

    Add ORMConfig & Board Entity, Repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmCoreModule.forRoot(typeORMConfig), BoardsModule],
 })
 export class AppModule {}
