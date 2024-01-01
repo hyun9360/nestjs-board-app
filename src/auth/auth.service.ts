82ef70264d1e552ce12698e623e34d1da1095607
hyun <zvg7777@gmail.com>
2024-01-01 21:59:26 +0900

    Update SignIn password JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,12 +9,14 @@ import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private jwtService: JwtService, // auth module 에 등록해줬기 때문에 사용 가능
   ) {}
 
   private async createUser(
@@ -41,12 +43,18 @@ export class AuthService {
     return this.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // 유저 토큰 생성 (Secret + Payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.signAsync(payload);
+
+      return { accessToken: accessToken };
     } else {
       throw new UnauthorizedException('login failed');
     }
