588682c5d09fec5857d86a7f8e5f1f4831a17072
hyun <zvg7777@gmail.com>
2024-01-01 20:46:10 +0900

    Add Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmCoreModule.forRoot(typeORMConfig), BoardsModule],
+  imports: [TypeOrmCoreModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
 })
 export class AppModule {}
