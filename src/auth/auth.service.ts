68a41e8d1148869b27c5c8cd966869177e625f9d
hyun <zvg7777@gmail.com>
2024-01-01 21:12:30 +0900

    Update SingUp Unique username

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -17,7 +21,13 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
     const user = this.userRepository.create({ username, password });
 
-    await this.userRepository.save(user);
+    try {
+      await this.userRepository.save(user);
+    } catch (e) {
+      if (e.code == '23505') {
+        throw new ConflictException('Existing username');
+      } else throw new InternalServerErrorException();
+    }
   }
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
