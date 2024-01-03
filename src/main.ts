1e624afc044bb0f3fc419963c814f7da884afa17
hyun <zvg7777@gmail.com>
2024-01-03 22:31:59 +0900

    Fix port config default setting

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<string>('server.port');
+  const port = configService.get<number>('server.port', 3000);
+
   console.log('configService:', configService);
-  await app.listen(port == undefined ? 3001 : port);
+
+  await app.listen(port);
   Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
